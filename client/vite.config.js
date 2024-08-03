df8203e8ed0da9b3da76997f478b47719d77627f
Isabella Fishman <fishman.bellam@gmail.com>
2024-08-03 00:15:43 -0500

    my last straw

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'  // Ensure Vite outputs to the 'dist' directory
+    outDir: 'boobs'  // Ensure Vite outputs to the 'dist' directory
   }
 })
